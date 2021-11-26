e447c419cdecd88584becb819a4e6c81e130572f
¨Alberto <albizan@hotmail.it>
2021-11-26 12:37:26 +0100

    Code refactoring and clean-up

--- book/server.js
+++ book/server.js
@@ -1,26 +1,6 @@
-if (process.env.NODE_ENV !== "production") {
-  // Load env variables from local .env file
-  console.log("Loading local env vars\n");
-  require("dotenv").config();
-}
 const fastify = require("fastify")({ logger: false });
-const { v4: uuidv4 } = require("uuid");
-const { getBooksController, getBookController } = require("./controller");
-const kafka = require("./kafka");
-const knex = require("./database");
-
-const bookSchema = {
-  type: "object",
-  properties: {
-    author: {
-      type: "string",
-    },
-    title: {
-      type: "string",
-    },
-  },
-  required: ["author", "title"],
-};
+const { getBooksController, getBookController, postBookController, updateBookController, deleteBookController } = require("./controller");
+const { bookSchema } = require("./schema");
 
 // Declare routes
 fastify.get("/book", getBooksController);
@@ -28,58 +8,17 @@ fastify.get("/book", getBooksController);
 fastify.get("/book/:id", getBookController);
 
 fastify.post("/book", {
-  handler: async (request, reply) => {
-    // console.log(request.body);
-    knex("book")
-      .insert({ ...request.body, id: uuidv4() })
-      .then((res) => {
-        reply.statusCode = 201;
-        reply.send(request.body);
-      })
-      .catch((err) => {
-        reply.sendCode = 500;
-      });
-  },
+  handler: postBookController,
   schema: {
     body: bookSchema,
   },
 });
 
-fastify.put("/book/:id", async (request, reply) => {
-  knex("book")
-    .where({ id: request.params.id })
-    .update(request.body, ["id"])
-    .then((affectedRows) => {
-      reply.statusCode = 200;
-      reply.send(affectedRows ? { affected: affectedRows.length } : { affected: 0 });
-    });
+fastify.put("/book/:id", {
+  handler: updateBookController,
+  schema: bookSchema,
 });
 
-fastify.delete("/book/:id", async (request, reply) => {
-  knex("book")
-    .where({ id: request.params.id })
-    .delete(["id"])
-    .then((returnings) => {
-      // console.log(returnings)
-      reply.statusCode = 204;
-      reply.send();
-    });
-});
+fastify.delete("/book/:id", deleteBookController);
 
-const start = async () => {
-  // Kafka configuration
-  await kafka.connect();
-  console.log("Connected to Kafka cluster\n");
-
-  try {
-    await fastify.listen({
-      port: process.env.SERVER_PORT,
-      host: "0.0.0.0", // Default is 127.0.0.1, to be avoided because it refuses connections from other containers or from the main host
-    });
-    console.log(`Server listening on port ${process.env.SERVER_PORT}\n`);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-};
-start();
+module.exports = fastify;
