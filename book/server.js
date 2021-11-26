3d16985b67b747a2d420e9b07619c96c42d9c634
¨Alberto <albizan@hotmail.it>
2021-11-26 01:05:26 +0100

    Start code clean-up

--- book/server.js
+++ book/server.js
@@ -1,23 +1,13 @@
-const fastify = require("fastify")({ logger: false });
-const { v4: uuidv4 } = require("uuid");
-const kafka = require("./kafka");
-
 if (process.env.NODE_ENV !== "production") {
   // Load env variables from local .env file
-  console.log("Loading local env vars...");
+  console.log("Loading local env vars\n");
   require("dotenv").config();
 }
-
-const knex = require("knex")({
-  client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-  },
-});
+const fastify = require("fastify")({ logger: false });
+const { v4: uuidv4 } = require("uuid");
+const { getBooksController, getBookController } = require("./controller");
+const kafka = require("./kafka");
+const knex = require("./database");
 
 const bookSchema = {
   type: "object",
@@ -33,51 +23,9 @@ const bookSchema = {
 };
 
 // Declare routes
-fastify.get("/book", async (request, reply) => {
-  knex("book")
-    .then(async (rows) => {
-      reply.statusCode = 200;
-      reply.send(rows);
+fastify.get("/book", getBooksController);
 
-      try {
-        await kafka.producer.connect();
-        console.log("Connected to kafka");
-        await kafka.producer.send({
-          topic: process.env.KAFKA_TOPIC_NAME,
-          messages: [
-            { value: `${rows.length} books retreived with a GET /book` },
-          ],
-        });
-      } catch (error) {
-        console.error(error);
-      }
-    })
-    .catch((err) => {
-      reply.sendCode = 500;
-      reply.send();
-    });
-});
-
-fastify.get("/book/:id", async (request, reply) => {
-  knex("book")
-    .where({
-      id: request.params.id,
-    })
-    .first()
-    .then((rows) => {
-      if (rows) {
-        reply.statusCode = 200;
-        reply.send(rows);
-      } else {
-        reply.statusCode = 404;
-        reply.send();
-      }
-    })
-    .catch((err) => {
-      reply.statusCode = 500;
-      reply.send();
-    });
-});
+fastify.get("/book/:id", getBookController);
 
 fastify.post("/book", {
   handler: async (request, reply) => {
@@ -103,9 +51,7 @@ fastify.put("/book/:id", async (request, reply) => {
     .update(request.body, ["id"])
     .then((affectedRows) => {
       reply.statusCode = 200;
-      reply.send(
-        affectedRows ? { affected: affectedRows.length } : { affected: 0 }
-      );
+      reply.send(affectedRows ? { affected: affectedRows.length } : { affected: 0 });
     });
 });
 
@@ -120,21 +66,19 @@ fastify.delete("/book/:id", async (request, reply) => {
     });
 });
 
-// Start the server!
 const start = async () => {
   // Kafka configuration
-  console.log("Connecting to kafka brokers...");
   await kafka.connect();
-  kafka.createTopic(process.env.KAFKA_TOPIC_NAME);
+  console.log("Connected to Kafka cluster\n");
 
   try {
-    console.log("Starting server on port", process.env.SERVER_PORT);
     await fastify.listen({
       port: process.env.SERVER_PORT,
       host: "0.0.0.0", // Default is 127.0.0.1, to be avoided because it refuses connections from other containers or from the main host
     });
+    console.log(`Server listening on port ${process.env.SERVER_PORT}\n`);
   } catch (err) {
-    fastify.log.error(err);
+    console.error(err);
     process.exit(1);
   }
 };
