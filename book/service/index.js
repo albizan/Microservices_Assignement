bdf7a8c17cdc9b50abf8dd1399ffc8a9d8eebbac
¨Alberto <albizan@hotmail.it>
2021-11-26 12:36:31 +0100

    Added services

--- book/service/index.js
+++ book/service/index.js
@@ -1,4 +1,5 @@
 const db = require("../database");
+const { v4: uuidv4 } = require("uuid");
 
 const getBooks = async () => {
   try {
@@ -18,4 +19,34 @@ const getBook = async (id) => {
   }
 };
 
-module.exports = { getBooks, getBook };
+const createBook = async (bookDTO) => {
+  try {
+    const newBook = {
+      id: uuidv4(),
+      ...bookDTO,
+    };
+    await db.insert(newBook).into("book");
+    return newBook;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const updateBook = async (id, updateBookDTO) => {
+  try {
+    const affectedRows = await db("book").where({ id }).update(updateBookDTO);
+    return affectedRows;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deleteBook = async (id) => {
+  try {
+    await db("book").where({ id }).del();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = { getBooks, getBook, createBook, updateBook, deleteBook };
