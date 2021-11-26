1e69ac974a4a40103a232568910f9db62479b6f6
Alberto Zanotti <albizan@hotmail.it>
2021-11-26 17:50:05 +0100

    Use logger

--- book/App.js
+++ book/App.js
@@ -1,18 +1,19 @@
 const server = require("./server");
 const kafka = require("./kafka");
+const logger = require("./logger");
 
 class App {
   async start() {
     try {
       // Kafka configuration
       await kafka.connect();
-      console.log("Connected to Kafka cluster\n");
+      logger.info("Connected to Kafka cluster");
 
       await server.listen({
         port: process.env.SERVER_PORT,
         host: "0.0.0.0", // Default is 127.0.0.1, to be avoided because it refuses connections from other containers or from the main host
       });
-      console.log(`Server listening on port ${process.env.SERVER_PORT}\n`);
+      logger.info(`Server listening on port ${process.env.SERVER_PORT}`);
     } catch (error) {
       console.error(err);
       process.exit(1);
--- book/service/index.js
+++ book/service/index.js
@@ -1,5 +1,6 @@
 const db = require("../database");
 const { v4: uuidv4 } = require("uuid");
+const logger = require("../logger");
 
 const getBooks = async () => {
   try {
@@ -13,6 +14,10 @@ const getBooks = async () => {
 const getBook = async (id) => {
   try {
     const book = await db.select("*").from("book").where({ id }).first();
+    logger.info("Requested book with id: " + id);
+    if (!book) {
+      logger.info("Book with id " + id + " does not exist");
+    }
     return book;
   } catch (error) {
     console.error(error);
@@ -26,6 +31,7 @@ const createBook = async (bookDTO) => {
       ...bookDTO,
     };
     await db.insert(newBook).into("book");
+    logger.info("Created book with id: " + newBook.id);
     return newBook;
   } catch (error) {
     console.error(error);
@@ -35,6 +41,7 @@ const createBook = async (bookDTO) => {
 const updateBook = async (id, updateBookDTO) => {
   try {
     const affectedRows = await db("book").where({ id }).update(updateBookDTO);
+    logger.info("Updated book with id: " + id);
     return affectedRows;
   } catch (error) {
     console.error(error);
@@ -44,6 +51,7 @@ const updateBook = async (id, updateBookDTO) => {
 const deleteBook = async (id) => {
   try {
     await db("book").where({ id }).del();
+    logger.info("Deleted book with id: " + id);
   } catch (error) {
     console.error(error);
   }
