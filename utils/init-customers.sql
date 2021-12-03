6f2f159b03ed46d67d0d61b8cc5260ec68b44b22
Alberto Zanotti <albizan@hotmail.it>
2021-12-03 22:12:56 +0100

    Init file for customers_db

---
+++ utils/init-customers.sql
@@ -0,0 +1,9 @@
+\connect customer
+
+CREATE TABLE "customer"
+(
+    id character varying primary key,
+    "firstName" character varying,
+    "lastName" character varying,
+    "privateAddress" character varying
+);
