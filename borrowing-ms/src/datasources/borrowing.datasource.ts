6dace0eed5d29eb4b115b3698b46c15f801ca53c
Alberto Zanotti <albizan@hotmail.it>
2021-12-20 21:04:29 +0100

    Do not use borrowing entity

--- borrowing-ms/src/datasources/borrowing.datasource.ts
+++ borrowing-ms/src/datasources/borrowing.datasource.ts
@@ -1,11 +1,12 @@
 import { MongoClient, Collection, ObjectId } from "mongodb";
 import { Service } from "typedi";
 import BorrowingEntity from "../entities/borrowing.entity";
+import Borrowing from "../types/Borrowing";
 
 @Service()
 class BorrowingDatasource {
   client: MongoClient;
-  borrowingCollection: Collection<BorrowingEntity>;
+  borrowingCollection: Collection<Borrowing>;
   constructor() {
     this.client = new MongoClient(process.env.DB_URI);
   }
@@ -28,13 +29,17 @@ class BorrowingDatasource {
     return borrowing;
   }
 
-  async addBorrowing(b: BorrowingEntity) {
+  async addBorrowing(b: Borrowing) {
     try {
       const { insertedId } = await this.borrowingCollection.insertOne(b);
       return insertedId;
     } catch (error) {
       console.log(error);
     }
+  }
+
+  async getBorrowingsByBookId(bookId: string): Promise<Borrowing[]> {
+    return await this.borrowingCollection.find({ "book.id": bookId }).toArray();
   }
 }
 
--- borrowing-ms/src/services/borrowing.service.ts
+++ borrowing-ms/src/services/borrowing.service.ts
@@ -18,54 +18,36 @@ class BorrowingService {
   private customerService: CustomerService;
 
   async createBorrowing(addBorrowingInput: AddBorrowingInput) {
-    // Check if book is available in the library
     const { bookId, customerId } = addBorrowingInput;
-    const book = await this.bookService.retrieveBookById(bookId);
-    const customer = await this.customerService.retrieveCustomerById(customerId);
 
-    const entity = new BorrowingEntity(bookId, customerId);
+    // Check if book is available in the library
+    const book = await this.bookService.retrieveBookById(bookId);
+    if (!book) return;
+
+    // Check if customer is present in the database
+    const customer = await this.customerService.retrieveCustomerById(customerId);
+    if (!customer) return;
+
+    const borrowing = new Borrowing(book, customer);
+
     // Add data to datasource
-    const _id = await this.dataSource.addBorrowing(entity);
-    entity._id = _id;
-    const borrowing = new Borrowing();
-    borrowing.mapFromModel(entity);
-    borrowing.addBook(book);
-    borrowing.addCustomer(customer);
+    borrowing._id = await this.dataSource.addBorrowing(borrowing);
+
     return borrowing;
   }
 
   async findBorrowingById(id: string): Promise<Borrowing> {
-    const b = await this.dataSource.findBorrowingById(id);
-    const bookId = b.bookId;
-    const customerId = b.customerId;
-
-    const book = await this.bookService.retrieveBookById(bookId);
-    const customer = await this.customerService.retrieveCustomerById(customerId);
-    const borrowing = new Borrowing();
-    borrowing.mapFromModel(b);
-    borrowing.addBook(book);
-    borrowing.addCustomer(customer);
-
+    const borrowing = await this.dataSource.findBorrowingById(id);
     return borrowing;
   }
 
   async getAllBorrowings(): Promise<Borrowing[]> {
-    const borrowingsFromDB = await this.dataSource.getAllBorrowings();
-    const borrowings: Borrowing[] = [];
-    for (let b of borrowingsFromDB) {
-      const bookId = b.bookId;
-      const customerId = b.customerId;
-      const book = await this.bookService.retrieveBookById(bookId);
-      const customer = await this.customerService.retrieveCustomerById(customerId);
+    const allBorrowings = await this.dataSource.getAllBorrowings();
+    return allBorrowings;
+  }
 
-      const borrowing = new Borrowing();
-      borrowing.mapFromModel(b);
-      borrowing.addBook(book);
-      borrowing.addCustomer(customer);
-      borrowings.push(borrowing);
-    }
-
-    return borrowings;
+  async getBorrowingsByBookId(bookId: string): Promise<Borrowing[]> {
+    return await this.dataSource.getBorrowingsByBookId(bookId);
   }
 }
 
