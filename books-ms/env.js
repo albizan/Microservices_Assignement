f2fb85420f795306acf40da0abec9b3d77749096
Alberto Zanotti <albizan@hotmail.it>
2021-12-20 16:07:19 +0100

    Config .env path

--- books-ms/env.js
+++ books-ms/env.js
@@ -1,5 +1,7 @@
+const { resolve } = require("path");
+
 if (process.env.NODE_ENV !== "production") {
   // Load env variables from local .env file
   console.log("Loading local env vars\n");
-  require("dotenv").config();
+  require("dotenv").config({ path: resolve(__dirname, "./.env") });
 }
